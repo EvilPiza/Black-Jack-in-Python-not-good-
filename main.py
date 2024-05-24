c0744cf2e6622f440a948a178e220c13e5aa0240
EvilPiza <140107536+EvilPiza@users.noreply.github.com>
2024-05-24 10:29:23 -0500

    Balance Changes + End Screen Consistency
    The bot now has a luck buff, and the "Draw" screen how tells what total y'all had, kind of useless but I like it.

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ import random
 
 plr_amount = 0
 maximum_amount = 21 #you can change this ig
-bot1 = random.randint(5, 10)
+bot1 = random.randint(7, 10)
 bot2 = random.randint(7, 11)
 bot_amount = bot1 + bot2
 print("The bot's first card is a " + str(bot1))
 def action():
     global plr_amount
     global maximum_amount
+    global bot_amount
     while plr_amount < maximum_amount:
         inp = str(input("Action: "))
         if inp == "hit" or inp == "h":
@@ -21,17 +22,28 @@ def action():
             stay()
     if plr_amount > maximum_amount:
         pass
-        
+    if plr_amount == maximum_amount:
+        if plr_amount > bot_amount:
+            print("You win!")
+            print(" ")
+            print("Player Amount: " + str(plr_amount))
+            print("Bot Amount: " + str(bot_amount))
+        if plr_amount == bot_amount:
+            print("Draw!")
+            print(" ")
+            print("Player Amount: " + str(plr_amount))
+            print("Bot Amount: " + str(bot_amount))
 def hit():
     r = random.randint(1,11)
     global plr_amount
     global maximum_amount
     global bot1
     if plr_amount < maximum_amount:
-        plr_amount += r
+        plr_amount += r #change r to 21 to always win btw
         print("You pulled a " + str(r))
         print(" ")
         print("Your total is " + str(plr_amount))
+        print(" ")
     if plr_amount > maximum_amount:
         print("You lost!")
 def stay():
@@ -49,6 +61,9 @@ def stay():
         print("Bot Amount: " + str(bot_amount))
     if plr_amount == bot_amount:
         print("Draw!")
+        print(" ")
+        print("Player Amount: " + str(plr_amount))
+        print("Bot Amount: " + str(bot_amount))
         
 action()
     
